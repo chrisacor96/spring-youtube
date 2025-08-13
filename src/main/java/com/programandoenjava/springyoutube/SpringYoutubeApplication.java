a346bb460344a512401e1208a922020aae7564fe
Christian Rodriguez <chris.acor96@gmail.com>
2025-08-13 15:32:11 -0500

    First commit

---
+++ src/main/java/com/programandoenjava/springyoutube/SpringYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.springyoutube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringYoutubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programandoenjava/springyoutube/SpringYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programandoenjava.springyoutube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
